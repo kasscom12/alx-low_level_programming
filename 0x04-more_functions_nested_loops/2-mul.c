0a2b53635e8ebe17eb5c72d7462ac3a96587d23f
kasscom12 <kassahutadele2021@gmail.com>
2022-09-15 13:50:02 -0700

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,20 +1,13 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
- * print_numbers - function that print all numbers from
- * 0 to 9
- *
- * Return: returns nothing
+ * mul - multiplies two numbers
+ * @a: first number
+ * @b: second number
+ * Return: returns result
  */
 
-void print_numbers(void)
+int mul(int a, int b)
 {
-	int n;
-
-	for (n = 48; n < 58; n++)
-	{
-		putchar(n);
-	}
-	putchar(10);
+	return (a * b);
 }

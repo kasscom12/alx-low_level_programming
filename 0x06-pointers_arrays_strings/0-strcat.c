f331ecd39619dceae81187b49629fb274854a2b3
kasscom12 <kassahutadele2021@gmail.com>
2022-09-21 13:06:28 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,24 +7,17 @@
  * Return: return value of dest
  */
 
-char *_srcat(char *dest, char *src)
+char *_strcat(char *dest, char *src)
 {
-	int i;
-	int j;
 
-	i = 0;
-	j = 0;
+	int index = 0, dest_len = 0;
 
-	while (dest[i] != '\0')
-	{
-		i++;
-	}
-	while (src[j] != '\0')
-			{
-	dest[i] = src[j];
-	j++;
-	i++;
-}
-dest[i] = '\0';
+
+	while (dest[index++])
+		dest_len++;
+	
+	for (index = 0; src[index]; index++)
+	dest[dest_len++] = src[index];
+
 return (dest);
 }

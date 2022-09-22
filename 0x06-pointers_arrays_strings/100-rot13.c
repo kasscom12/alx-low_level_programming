3f9f6c55c150f5d561764845381928215c3e97e7
kasscom12 <kassahutadele2021@gmail.com>
2022-09-22 14:32:45 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -16,12 +16,12 @@ char *rot13(char *s)
 	{
 		for (j = 0; j < 52; j++)
 		{
-				if (a[j] == *(s + i))
-				{
-					*(s + i) = b[j];
-					break;
-				}
+			if (a[j] == *(s + i))
+			{
+				*(s + i) = b[j];
+				break;
 			}
 		}
-		return (s);
+	}
+	return (s);
 }

addb21b54c98dc52264c3ee983fe109f650468fd
kasscom12 <kassahutadele2021@gmail.com>
2022-09-28 14:56:30 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,11 +7,11 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s)
+	if (* == '\0');
 	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
-	if (!*s)
 		_putchar('\0');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
 	}

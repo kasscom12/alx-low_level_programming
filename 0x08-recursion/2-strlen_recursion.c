20722d93b9181a2c782915ad0f27c3702e4e704d
kasscom12 <kassahutadele2021@gmail.com>
2022-09-28 15:25:13 -0700

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,13 +2,14 @@
 /**
  * _strlen_recursion - returns the length of a string
  * @s: the string
+ *
  * Return: int/ length of string
  */
 int _strlen_recursion(char *s)
 {
 	if (*s)
 	{
-		return (1 + strlen_recursion(s + 1));
+		return (1 + _strlen_recursion(s + 1));
 	}
 	else
 	{

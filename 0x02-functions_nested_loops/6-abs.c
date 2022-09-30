a3bfd9c720cadf074064aa09382180e29fe6eb8a
kasscom12 <kassahutadele2021@gmail.com>
2022-09-30 11:26:42 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,39 @@
+int _putchar(char c);
+
+int_islower (int c);
+
+int_isalpha (int c);
+
+int_abs (int n);
+
+int_isupper (int c) :
+
+	int_isdigit (int c);
+
+	int_strlen (char *s);
+
+	void _puts(char *s);
+
+	char *_strcpy(char *idest, char *src);
+
+	 (char
+
+	  int _atoi(char*s);
+
+	  char *_strcat (char *dest,char *sre);
+
+	  char*_strncat(char *dest, char *src, int n):
+
+	  char* (char *dest, char *src, int n):
+
+	  int_stremp (char *s1, char *$2) :
+
+	  char*_memset (char
+
+		  char *_memcpy (char *dest,char
+
+			  char *_strchr(char *S char c);
+
+			  char *_strstr(char *haystack, char *needle) :
+
+#endif
